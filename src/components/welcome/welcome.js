acc07974d08848b3e7e0aab11ba1086b74238806
Thais Cristine <this.cristine06@gmail.com>
2020-11-16 02:31:27 +0200

    feat: adding basic welcome layout

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -3,8 +3,8 @@ import imgLiaIntro from '../../assets/lia_intro.png';
 
 function welcome() {
   return (
-    <div>
-        <img src={imgLiaIntro} alt="Lia"/>
+    <div style={{ top: '-120px', left: '-20%', position: 'absolute' }}>
+        <img src={imgLiaIntro} alt="Lia" style={{ maxWidth: '800px' }} />
     </div>
   );
 }
